c0ec37667290af4e43490bfd22923c488ede87d4
Jami Kazmi <58857825+JamiKazmi@users.noreply.github.com>
2020-02-09 15:30:02 +0500

    Initial Commit

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
-# for jupyter notebook
-# % matplotlib inline
 
 # Analyzing data
 df = pd.read_csv('files/FullData.csv')
